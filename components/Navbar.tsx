eb64d53679ec2bd438e7a2385e5def8cb80a7c3d
Lee <leecook808@gmail.com>
2023-11-06 18:09:35 -0800

    wip

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,32 @@
+'use client'
+
 import React from 'react';
 import Image from "next/image";
 import NavBanner from "../public/imgs/nav/nav_banner.svg";
 import Link from "next/link";
 import styles from '../styles/navbar.module.css';
+import {usePathname} from 'next/navigation';
 
 const Navbar = () => {
+    const pathname = usePathname();
     return (
         <div>
-            <div className={styles.bannerBackgroundImage} />
+            <div className={styles.bannerBG} />
             <nav className={styles.container}>
                 <Image src={NavBanner} alt="Navbanner" className={styles.bannerImage} />
                 <div className={styles.menu}>
                     <ul>
-                        <Link href="/home">
-                            <li>Home</li>
+                        <Link href="/">
+                            <li className={pathname == "/" ? styles.currentPage : ""}>Home</li>
                         </Link>
                         <Link href="/card_specs">
-                            <li>Card Specs</li>
+                            <li className={pathname == "/card_specs" ? styles.currentPage : ""}>Card Specs</li>
                         </Link>
                         <Link href="/how_to_play">
-                            <li>How To Play</li>
+                            <li className={pathname == "/how_to_play" ? styles.currentPage : ""}>How To Play</li>
                         </Link>
                         <Link href="/opensea">
-                            <li>Opensea</li>
+                            <li className={pathname == "/opensea" ? styles.currentPage : ""}>Opensea</li>
                         </Link>
                     </ul>
                 </div>
