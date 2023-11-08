62d0344bfaf4eb13cb0a6c9d6c80e1bb97531f9d
Lee <leecook808@gmail.com>
2023-11-07 23:45:36 -0800

    number colors

--- components/card_specs/Card1.tsx
+++ components/card_specs/Card1.tsx
@@ -8,19 +8,40 @@ const Card1 = () => {
         <div className={styles.container}>
             <div className={styles.containerTextBG}>
                 <div className={styles.containerText}>
-                    <h1>1. Card Name</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #FD65C2FF", background:"#FD65C2FF"}}>1</h1>
+                        <h1>Card Name</h1>
+                    </div>
                     <p>This is the name of the card.</p>
-                    <h1>2. Rarity</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #6CC540FF", background:"#6CC540FF"}}>2</h1>
+                        <h1>Rarity</h1>
+                    </div>
                     <p>The nameplate color indicates the rarity of the monsters.</p>
-                    <h1>3. Theme</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #30D7C6FF", background:"#30D7C6FF"}}>3</h1>
+                        <h1>Theme</h1>
+                    </div>
                     <p>All monsters belong to one of the 7 themes.</p>
-                    <h1>4. Health Points</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #7182ACFF", background:"#7182ACFF"}}>4</h1>
+                        <h1>Health Points</h1>
+                    </div>
                     <p>HP represents the health points of the monster. The higher the HP the harder to take down!</p>
-                    <h1>5. Type</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #FD2600FF", background:"#FD2600FF"}}>5</h1>
+                        <h1>Type</h1>
+                    </div>
                     <p>Cards can be one of three types: Monster, Healer, or Trickster.</p>
-                    <h1>6. Attribute</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #FDD700FF", background:"#FDD700FF"}}>6</h1>
+                        <h1>Attribute</h1>
+                    </div>
                     <p>There are 4 Attributes in total: Ripe, Sweet, Bitter, and Rotten. Look out for the NFTs that have multiple!</p>
-                    <h1>7. Technique</h1>
+                    <div className={styles.containerNumber}>
+                        <h1 className={styles.number} style={{border:"3px solid #ED13AAFF", background:"#ED13AAFF"}}>7</h1>
+                        <h1>Technique</h1>
+                    </div>
                     <p>Effects of monsters are written here. When reading the Technique, look for key details such as attack power, healing capabilities, and any special conditions or limitations.</p>
                 </div>
             </div>
